d149dfb719079f1d6ece275030c66bd4916471f6
Matheus Alfonso <matheusalff@gmail.com>
2021-08-05 14:05:23 -0300

    Adicionado MVP da pagina de detalhes do servico

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import CadastrarServico from './components/CadastrarServico';
+import DetalhesServico from './components/DetalhesServico';
 
 export default class App extends React.Component {
 	render() {
 	  return (
 		<div>
-			<CadastrarServico></CadastrarServico>
+			<DetalhesServico></DetalhesServico>
 		</div>
 	  );
 	}
