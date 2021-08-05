f039136212d6911aea19be5551e3ddbc556ef9af
Matheus Alfonso <matheusalff@gmail.com>
2021-08-05 14:12:46 -0300

    Adicionado MVP do Carrinho

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import CadastrarServico from './components/CadastrarServico';
 import DetalhesServico from './components/DetalhesServico';
+import Carrinho from './components/Carrinho';
 
 export default class App extends React.Component {
 	render() {
 	  return (
 		<div>
-			<DetalhesServico></DetalhesServico>
+			<Carrinho></Carrinho>
 		</div>
 	  );
 	}
